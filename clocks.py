06bb5544ab3dad10b30ae0c67ae1a603259350c7
fakeroot72 <fakeroot72@gmail.com>
2025-11-26 09:34:11 +0300

    Update clocks.py
    minor readability changes

--- clocks.py
+++ clocks.py
@@ -1,86 +1,93 @@
 #!/usr/bin/python
 import sys
 
-def ending(n, type):
+def ending(number, type):
     if(type != "m" and type != "f"):
         return "err"
 
-    n = abs(n)
-    if(n % 10 > 4 or n % 10 == 0 or 10 < n % 100 < 20):
+    number = abs(number)
+    if(number % 10 > 4 or number % 10 == 0 or 10 < number % 100 < 20):
         return "ов" if type == 'm' else ""
-    elif(n % 10 == 1):
+    elif(number % 10 == 1):
         return "" if type == 'm' else "а"
     else:
         return "а" if type == 'm' else "ы"
 
-def mins_to_str(hr, min, total_mins):
-    if(total_mins != None):
-        hr = total_mins // 60
-        min = total_mins % 60
+def time_to_str(hour, minute, total_minutes):
+    if(total_minutes != None):
+        hour = total_minutes // 60
+        minute = total_minutes % 60
     else:
-        total_mins = hr * 60 + min
+        total_minutes = hour * 60 + minute
 
     daytime = ""
-    hr_str = ""
-    min_str = ""
-    if(total_mins == 0):
+    hour_str = ""
+    minutes_str = ""
+    if(total_minutes == 0):
         return "полночь"
 
-    elif(0 < total_mins < 6 * 60):
+    elif(0 < total_minutes < 6 * 60):
         daytime = "ночи"
-        hr_str = str(hr) + " час" + ending(hr, 'm')
-        min_str = str(min) + " минут" + ending(min, 'f')
+        hour_str = str(hour) + " час" + ending(hour, 'm')
+        minutes_str = str(minute) + " минут" + ending(minute, 'f')
 
-    elif(6 * 60 <= total_mins < 12 * 60):
+    elif(6 * 60 <= total_minutes < 12 * 60):
         daytime = "утра"
-        hr_str = str(hr) + " час" + ending(hr, 'm')
-        min_str = str(min) + " минут" + ending(min, 'f')
+        hour_str = str(hour) + " час" + ending(hour, 'm')
+        minutes_str = str(minute) + " минут" + ending(minute, 'f')
 
-    elif(total_mins == 12 * 60):
+    elif(total_minutes == 12 * 60):
         return "полдень"
 
-    elif(12 * 60 < total_mins < 18 * 60):
+    elif(12 * 60 < total_minutes < 18 * 60):
         daytime = "дня"
-        hr_clock = hr if hr == 12 else hr - 12
-        hr_str = str(hr_clock) + " час" + ending(hr_clock, 'm')
-        min_str = str(min) + " минут" + ending(min, 'f')
+        hour_clock = hour if hour == 12 else hour - 12
+        hour_str = str(hour_clock) + " час" + ending(hour_clock, 'm')
+        minutes_str = str(minute) + " минут" + ending(minute, 'f')
 
-    elif(18 * 60 <= total_mins < 24 * 60):
+    elif(18 * 60 <= total_minutes < 24 * 60):
         daytime = "вечера"
-        hr_str = str(hr - 12) + " час" + ending(hr - 12, 'm')
-        min_str = str(min) + " минут" + ending(min, 'f')
+        hour_str = str(hour - 12) + " час" + ending(hour - 12, 'm')
+        minutes_str = str(minute) + " минут" + ending(minute, 'f')
 
     else:
         return ""
 
-    if(min == 0):
-        return hr_str + " " + daytime + " ровно"
+    if(minute == 0):
+        return hour_str + " " + daytime + " ровно"
 
-    return hr_str + " " + min_str + " " + daytime
+    return hour_str + " " + minutes_str + " " + daytime
 
 def main():
     time_str = input().split()
+    if(time_str == [] or time_str[0] == "help"):
+        print("Требуется ввести время в формате xx yy где xx от 0 до 23 и yy от 0 до 59.")
+        return 2
     if(len(time_str) != 2):
         print("Введены недопустимые данные: присутствуют посторонние символы")
         return 2
 
+    hour = 0
+    minute = 0
+
     try:
-        hr = int(time_str[0])
-        min = int(time_str[1])
+        hour = int(time_str[0])
+        minute = int(time_str[1])
     except ValueError:
         print("Введены недопустимые данные: присутствуют посторонние символы")
         return 2
 
-    if(not 0 <= hr < 24):
+    if(not 0 <= hour < 24):
         print("Введены недопустимые данные: часы должны быть от 0 до 23")
         return 2
 
-    if(not 0 <= min < 60):
+    if(not 0 <= minute < 60):
         print("Введены недопустимые данные: часы должны быть от 0 до 59")
         return 2
 
-    print(mins_to_str(hr, min, None))
+    print(time_to_str(hour, minute, None))
     return 0
 
 if(__name__ == "__main__"):
     sys.exit(main())
+
